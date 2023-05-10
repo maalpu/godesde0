eb613894352ae74c83a2d488653054062c489fbe
Mario <mariopuebla@gmail.com>
2023-05-10 17:20:24 -0300

    agregamos funcion ConviertoATexto

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/maalpu/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
-	variables.RestoVariables()
+	// variables.MuestroEnteros()
+	// variables.RestoVariables()
+	Estado, texto := variables.ConviertoATexto(13035)
+	fmt.Println(texto, Estado)
 }
--- variables/resto.go
+++ variables/resto.go
@@ -2,6 +2,7 @@ package variables
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -18,4 +19,9 @@ func RestoVariables() {
 	fmt.Println("Estado        = ", Estado)
 	fmt.Println("Sueldo        = ", Sueldo)
 	fmt.Println("Fecha         = ", Fecha)
+}
+
+func ConviertoATexto(numero int) (bool, string) {
+	texto := strconv.Itoa(numero)
+	return true, texto
 }
